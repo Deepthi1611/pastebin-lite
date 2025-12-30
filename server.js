8a6c5b839a84d708555be766c0504cc92db3d0cb
Deepthi Purijala <deepthipurijala@gmail.com>
2025-12-30 13:38:35 +0530

    added html related logic

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const path = require("path");
 const { connectRedis } = require("./redisClient");
 const index = require("./routes/index");
+const htmlRoutes = require("./routes/html");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// body parser middleware
 app.use(express.json());
+// to support URL-encoded bodies
+app.use(express.urlencoded({ extended: false }));
 
 connectRedis()
   .then(() => console.log("Redis connected"))
@@ -15,10 +20,19 @@ connectRedis()
   });
 
 app.use("/api", index);
+app.use(htmlRoutes);
+
+// app.get("/", (req, res) => {
+//   res.json({ message: "Pastebin-Lite server running" });
+// });
 
 app.get("/", (req, res) => {
-  res.json({ message: "Pastebin-Lite server running" });
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
+// app.get("/", (req, res) => {
+//   res.send("<h1>HTML WORKS</h1>");
+// });
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
